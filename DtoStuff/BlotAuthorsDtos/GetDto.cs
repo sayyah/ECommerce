91fa225b4022ac980be22bdf3c7b9964fe63a646
Golnaz Najafi <blume.sein@gmail.com>
2023-01-23 22:07:14 +0330

    dddd

---
+++ DtoStuff/BlotAuthorsDtos/GetDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoStuff.BlotAuthorsDtos
+{
+    public class GetDto
+    {
+        public int CurrentPage { get; }
+        public int TotalPages { get; }
+        public int PageSize { get; }
+        public int TotalCount { get; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+        public string? Search { get; set; }
+    }
+}
