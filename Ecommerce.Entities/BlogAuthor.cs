0a4d29c1e23fceefc701996679c6c89ce66fc93e
Abdorreza Joe Afshany <joe.afshany@gmail.com>
2023-11-06 14:17:21 +0330

    Change Name to FarsiName
    Allow Only English and Digits

--- Ecommerce.Entities/BlogAuthor.cs
+++ Ecommerce.Entities/BlogAuthor.cs
@@ -5,7 +5,7 @@ namespace Ecommerce.Entities;
 
 public class BlogAuthor : RootEntity
 {
-    [Display(Name = "نام")]
+    [Display(Name = "نام فارسی")]
     [StringLength(50, MinimumLength = 3, ErrorMessage = @"حداقل 3 و حداکثر 50 کاراکتر")]
     [Required(ErrorMessage = @"{0} را وارد کنید")]
     public string Name { get; set; }
