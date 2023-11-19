bf72de95f93b4b7952d8b7f05a0363dad6e237e1
Alireza Sayyah <sayyah.alireza@gmail.com>
2023-11-20 00:09:28 +0330

    close #599 repaire snapshot

--- ECommerce.Infrastructure.Repository/StateRepository.cs
+++ ECommerce.Infrastructure.Repository/StateRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace ECommerce.Infrastructure.Repository;
+﻿using ECommerce.Infrastructure.DataContext;
+
+namespace ECommerce.Infrastructure.Repository;
 
 public class StateRepository : AsyncRepository<State>, IStateRepository
 {
