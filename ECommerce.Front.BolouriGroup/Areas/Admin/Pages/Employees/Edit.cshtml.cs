6cd070cb72ccf7986b1be9ba555cdf5d355d1ba0
Pourya Pourbagheri <pouryapb77@gmail.com>
2023-11-19 17:52:57 +0330

    Fixes #602 Department missing everywhere

--- ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Employees/Edit.cshtml.cs
+++ ECommerce.Front.BolouriGroup/Areas/Admin/Pages/Employees/Edit.cshtml.cs
@@ -1,24 +1,34 @@
 ﻿using ECommerce.Services.IServices;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ECommerce.Front.BolouriGroup.Areas.Admin.Pages.Employees;
 
 public class EditModel : PageModel
 {
     private readonly IEmployeeService _employeeService;
+    private readonly IDepartmentService _departmentService;
 
-    public EditModel(IEmployeeService employeeService)
+    public EditModel(IEmployeeService employeeService, IDepartmentService departmentService)
     {
         _employeeService = employeeService;
+        _departmentService = departmentService;
     }
 
     [BindProperty] public Employee Employee { get; set; }
     [TempData] public string Message { get; set; }
     [TempData] public string Code { get; set; }
+    public SelectList Departments { get; set; }
 
     public async Task OnGet(int id)
     {
         var result = await _employeeService.GetById(id);
         Employee = result.ReturnData;
+        await Init();
+    }
+
+    private async Task Init() {
+        var departments = (await _departmentService.GetAll()).ReturnData;
+        Departments = new SelectList(departments, nameof(Department.Id), nameof(Department.Title));
     }
 
     public async Task<IActionResult> OnPost()
