ab1a4bcfb53b5eb82e21084bbbfeef2fd1e187d4
Saman <samanmozhdehi@gmail.com>
2023-11-20 08:53:29 +0330

    Fixes #559 Hundred Discount

---
+++ Ecommerce.Entities/ViewModel/CategoryParentViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Ecommerce.Entities.ViewModel;
+
+public class CategoryParentViewModel
+{
+    public int Id { get; set; }
+    public int Depth { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public bool Checked { get; set; }
+    public int DisplayOrder { get; set; }
+    public Discount? Discount { get; set; }
+
+    public List<CategoryParentViewModel> Children { get; set; }
+
+    public static implicit operator CategoryParentViewModel(Category x)
+    {
+        return new CategoryParentViewModel
+        {
+            Id = x.Id,
+            Depth = x.Depth,
+            Name = x.Name,
+            Path = x.Path,
+            DisplayOrder = x.DisplayOrder,
+            Discount = x.Discount
+        };
+    }
+    public static implicit operator Category(CategoryParentViewModel x)
+    {
+        return new Category
+        {
+            Id = x.Id,
+            Depth = x.Depth,
+            Name = x.Name,
+            Path = x.Path,
+            DisplayOrder = x.DisplayOrder,
+            Discount = x.Discount
+        };
+    }
+}
