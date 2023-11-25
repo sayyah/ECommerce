0127e4a886220c1a5b703f23eb0daf4eeb8c6cc8
Saman <samanmozhdehi@gmail.com>
2023-11-25 22:13:39 +0330

    Fixes #495 wishList

--- ECommerce.Repository.UnitTests/Base/BaseTests.cs
+++ ECommerce.Repository.UnitTests/Base/BaseTests.cs
@@ -1,4 +1,5 @@
-﻿using ECommerce.Infrastructure.DataContext;
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Infrastructure.DataContext;
 
 namespace ECommerce.Repository.UnitTests.Base;
 
@@ -15,6 +16,24 @@ public abstract class BaseTests : IDisposable
         CancellationToken = new CancellationToken();
         DbContext = Db.CreateDatabaseContext();
         HolooDbContext = Db.CreateHolooDatabaseContext();
+    }
+
+    public async Task<Product> GetProduct()
+    {
+        //Arrange
+        Brand brand = new Brand { Id = 1, Name = "brandName" };
+        Image image = new Image { Id = 1, Path = "imagePath", Name = "ImageName", Alt = "ImageAlt" };
+        Product product = new Product { Id = 1, Name = "productName", Url = "productUrl", BrandId = 1, Brand = brand, Images = new List<Image> { image } };
+        Price price = new Price { Id = 1, ProductId = product.Id };
+        product!.Prices!.Add(price!);
+        price.Product = product;
+
+        await DbContext.Brands.AddAsync(brand, CancellationToken);
+        await DbContext.Images.AddAsync(image, CancellationToken);
+        await DbContext.Products.AddAsync(product, CancellationToken);
+        await DbContext.Prices.AddAsync(price, CancellationToken);
+
+        return product;
     }
 
     public void Dispose()
