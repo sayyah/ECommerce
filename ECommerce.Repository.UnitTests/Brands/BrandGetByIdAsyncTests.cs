c679406912c24ccf880a2d4d9909634a10e5a0c3
Saman <samanmozhdehi@gmail.com>
2023-12-05 10:28:49 +0330

    Fixes #464 Brand Test

---
+++ ECommerce.Repository.UnitTests/Brands/BrandGetByIdAsyncTests.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.Domain.Entities;
+using Xunit;
+
+namespace ECommerce.Repository.UnitTests.Brands;
+
+public class BrandGetByIdAsyncTests : BrandBaseTests
+{
+    [Fact]
+    public async void GetByIdAsync_GetAddedEntityById_EntityExistsInRepository()
+    {
+        // Arrange
+        const int id = 1;
+        Brand brand = new()
+        {
+            Id = id,
+            Name = Guid.NewGuid().ToString(),
+            Url = Guid.NewGuid().ToString()
+        };
+        DbContext.Brands.Add(brand);
+        await DbContext.SaveChangesAsync();
+
+        // Act
+        var result = await BrandRepository.GetByIdAsync(CancellationToken, id);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async void GetByIdAsync_GetEntityByFalseId_ReturnNull()
+    {
+        // Arrange
+        const int id = 0;
+
+        // Act
+        var result = await BrandRepository.GetByIdAsync(CancellationToken, id);
+
+        // Assert
+        Assert.Null(result);
+    }
+}
