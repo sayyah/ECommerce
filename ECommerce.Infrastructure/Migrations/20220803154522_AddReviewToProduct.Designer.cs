5e3dba758083fe62a4adc2af9ff64bd0c153c2e4
Saman <samanmozhdehi@gmail.com>
2024-01-16 14:07:06 +0330

    Fixes #671 Discount Description

--- ECommerce.Infrastructure/Migrations/20220803154522_AddReviewToProduct.Designer.cs
+++ ECommerce.Infrastructure/Migrations/20220803154522_AddReviewToProduct.Designer.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ECommerce.Infrastructure.DataContext.DataContext;
+using ECommerce.Infrastructure.DataContext;
 
 #nullable disable
 
