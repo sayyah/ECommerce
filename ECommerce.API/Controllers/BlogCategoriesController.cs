e0a8f75d5cd0b50bfd2cd1c1682f4151c2a54f08
Saman <samanmozhdehi@gmail.com>
2024-01-27 09:31:25 +0330

    Fixes #661 cqrs blogCategories

--- ECommerce.API/Controllers/BlogCategoriesController.cs
+++ ECommerce.API/Controllers/BlogCategoriesController.cs
@@ -1,175 +1,174 @@
-﻿namespace ECommerce.API.Controllers;
+﻿using ECommerce.API.DataTransferObject.BlogCategories;
+using ECommerce.API.Utilities;
+using ECommerce.Application.Services.BlogCategories.Queries;
+using ECommerce.Application.Services.BlogCategories.Results;
+using ECommerce.Application.Services.Interfaces;
+
+
+namespace ECommerce.API.Controllers;
 
 [Route("api/[controller]/[action]")]
 [ApiController]
-public class BlogCategoriesController(IUnitOfWork unitOfWork,
-        ILogger<BlogCategoriesController> logger)
-    : ControllerBase
+public class BlogCategoriesController(IDtoMapper mapper, ILogger<BlogsController> logger) : ControllerBase
 {
-    private readonly IBlogCategoryRepository _blogCategoryRepository = unitOfWork.GetRepository<BlogCategoryRepository,BlogCategory>();
-
     [HttpGet]
-    public IActionResult GetAllWithPagination([FromQuery] PaginationParameters paginationParameters)
+    public async Task<ActionResult<ICollection<BlogCategoryDto>>> GetAllWithPagination(
+        [FromQuery] GetBlogCategoriesQueryDto getBlogCategoriesQueryDto,
+        [FromServices] IQueryHandler<GetBlogCategoriesQuery, PagedList<BlogCategoryResult>> queryHandler)
     {
         try
         {
-            if (string.IsNullOrEmpty(paginationParameters.Search)) paginationParameters.Search = "";
-            var entity = _blogCategoryRepository.Search(paginationParameters);
-            var paginationDetails = new PaginationDetails
+            if (string.IsNullOrEmpty(getBlogCategoriesQueryDto.PaginationParameters.Search))
             {
-                TotalCount = entity.TotalCount,
-                PageSize = entity.PageSize,
-                CurrentPage = entity.CurrentPage,
-                TotalPages = entity.TotalPages,
-                HasNext = entity.HasNext,
-                HasPrevious = entity.HasPrevious,
-                Search = paginationParameters.Search
-            };
-            return Ok(new ApiResult
-            {
-                PaginationDetails = paginationDetails,
-                Code = ResultCode.Success,
-                ReturnData = entity
-            });
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
-        }
-    }
+                getBlogCategoriesQueryDto.PaginationParameters.Search = "";
+            }
 
-    [HttpGet]
-    public async Task<ActionResult<BlogCategory>> GetById(int id, CancellationToken cancellationToken)
-    {
-        try
-        {
-            var result = await _blogCategoryRepository.GetByIdAsync(cancellationToken, id);
-            if (result == null)
-                return Ok(new ApiResult
-                {
-                    Code = ResultCode.NotFound
-                });
-
+            GetBlogCategoriesQuery query = mapper.Map<GetBlogCategoriesQuery>(getBlogCategoriesQueryDto);
+            var blogCategories = await queryHandler.HandleAsync(query);
+            PagedList<BlogCategoryDto> blogCategoryDto = mapper.Map<PagedList<BlogCategoryDto>>(blogCategories);
             return Ok(new ApiResult
             {
                 Code = ResultCode.Success,
-                ReturnData = result
+                ReturnData = blogCategoryDto
             });
         }
         catch (Exception e)
         {
             logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+            return Ok(new ApiResult { Code = ResultCode.DatabaseError });
         }
     }
 
+    //[HttpGet]
+        //public async Task<ActionResult<BlogCategory>> GetById(int id, CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        var result = await _blogCategoryRepository.GetByIdAsync(cancellationToken, id);
+        //        if (result == null)
+        //            return Ok(new ApiResult
+        //            {
+        //                Code = ResultCode.NotFound
+        //            });
 
-    [HttpGet]
-    public async Task<IActionResult> GetParents(int blogId, CancellationToken cancellationToken)
-    {
-        try
-        {
-            var result = await _blogCategoryRepository.Parents(blogId, cancellationToken);
-            if (result == null)
-                return Ok(new ApiResult
-                {
-                    Code = ResultCode.NotFound
-                });
+        //        return Ok(new ApiResult
+        //        {
+        //            Code = ResultCode.Success,
+        //            ReturnData = result
+        //        });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogCritical(e, e.Message);
+        //        return Ok(new ApiResult
+        //            { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+        //    }
+        //}
 
-            return Ok(new ApiResult
-            {
-                Code = ResultCode.Success,
-                ReturnData = result
-            });
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
-        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetParents(int blogId, CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        var result = await _blogCategoryRepository.Parents(blogId, cancellationToken);
+        //        if (result == null)
+        //            return Ok(new ApiResult
+        //            {
+        //                Code = ResultCode.NotFound
+        //            });
+
+        //        return Ok(new ApiResult
+        //        {
+        //            Code = ResultCode.Success,
+        //            ReturnData = result
+        //        });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogCritical(e, e.Message);
+        //        return Ok(new ApiResult
+        //            { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+        //    }
+        //}
+
+
+        //[HttpPost]
+        //[Authorize(Roles = "Admin,SuperAdmin")]
+        //public async Task<IActionResult> Post(BlogCategory? blogCategory, CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        if (blogCategory == null)
+        //            return Ok(new ApiResult
+        //            {
+        //                Code = ResultCode.BadRequest
+        //            });
+        //        blogCategory.Name = blogCategory.Name.Trim();
+
+        //        var repetitiveCategory =
+        //            await _blogCategoryRepository.GetByName(blogCategory.Name, blogCategory.Parent?.Id, cancellationToken);
+        //        if (repetitiveCategory != null)
+        //            return Ok(new ApiResult
+        //            {
+        //                Code = ResultCode.Repetitive,
+        //                Messages = new List<string> { "نام دسته تکراری است" }
+        //            });
+        //        _blogCategoryRepository.Add(blogCategory);
+        //        await unitOfWork.SaveAsync(cancellationToken);
+
+        //        return Ok(new ApiResult
+        //        {
+        //            Code = ResultCode.Success,
+        //        });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogCritical(e, e.Message);
+        //        return Ok(new ApiResult
+        //            { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+        //    }
+        //}
+
+        //[HttpPut]
+        //[Authorize(Roles = "Admin,SuperAdmin")]
+        //public async Task<ActionResult<bool>> Put(BlogCategory blogCategory, CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        _blogCategoryRepository.Update(blogCategory);
+        //        await unitOfWork.SaveAsync(cancellationToken);
+        //        return Ok(new ApiResult
+        //        {
+        //            Code = ResultCode.Success
+        //        });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogCritical(e, e.Message);
+        //        return Ok(new ApiResult
+        //            { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+        //    }
+        //}
+
+        //[HttpDelete]
+        //[Authorize(Roles = "SuperAdmin")]
+        //public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+        //{
+        //    try
+        //    {
+        //        await _blogCategoryRepository.DeleteById(id, cancellationToken);
+        //        await unitOfWork.SaveAsync(cancellationToken);
+        //        return Ok(new ApiResult
+        //        {
+        //            Code = ResultCode.Success
+        //        });
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        logger.LogCritical(e, e.Message);
+        //        return Ok(new ApiResult
+        //            { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
+        //    }
+        //}
     }
-
-
-    [HttpPost]
-    [Authorize(Roles = "Admin,SuperAdmin")]
-    public async Task<IActionResult> Post(BlogCategory? blogCategory, CancellationToken cancellationToken)
-    {
-        try
-        {
-            if (blogCategory == null)
-                return Ok(new ApiResult
-                {
-                    Code = ResultCode.BadRequest
-                });
-            blogCategory.Name = blogCategory.Name.Trim();
-
-            var repetitiveCategory =
-                await _blogCategoryRepository.GetByName(blogCategory.Name, blogCategory.Parent?.Id, cancellationToken);
-            if (repetitiveCategory != null)
-                return Ok(new ApiResult
-                {
-                    Code = ResultCode.Repetitive,
-                    Messages = new List<string> { "نام دسته تکراری است" }
-                });
-            _blogCategoryRepository.Add(blogCategory);
-            await unitOfWork.SaveAsync(cancellationToken);
-
-            return Ok(new ApiResult
-            {
-                Code = ResultCode.Success,
-            });
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
-        }
-    }
-
-    [HttpPut]
-    [Authorize(Roles = "Admin,SuperAdmin")]
-    public async Task<ActionResult<bool>> Put(BlogCategory blogCategory, CancellationToken cancellationToken)
-    {
-        try
-        {
-            _blogCategoryRepository.Update(blogCategory);
-            await unitOfWork.SaveAsync(cancellationToken);
-            return Ok(new ApiResult
-            {
-                Code = ResultCode.Success
-            });
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
-        }
-    }
-
-    [HttpDelete]
-    [Authorize(Roles = "SuperAdmin")]
-    public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
-    {
-        try
-        {
-            await _blogCategoryRepository.DeleteById(id, cancellationToken);
-            await unitOfWork.SaveAsync(cancellationToken);
-            return Ok(new ApiResult
-            {
-                Code = ResultCode.Success
-            });
-        }
-        catch (Exception e)
-        {
-            logger.LogCritical(e, e.Message);
-            return Ok(new ApiResult
-                { Code = ResultCode.DatabaseError, Messages = new List<string> { "اشکال در سمت سرور" } });
-        }
-    }
-}
---
+++ ECommerce.Application.Services/BlogCategories/Queries/GetBlogCategoriesQuery.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Application.Services.Blogs.Results;
+using ECommerce.Application.Services.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Application.Services.BlogCategories.Results;
+
+namespace ECommerce.Application.Services.BlogCategories.Queries;
+
+public class GetBlogCategoriesQuery : IQuery<PagedList<BlogCategoryResult>>
+{
+    public PaginationParameters PaginationParameters { get; set; } = new();
+}
