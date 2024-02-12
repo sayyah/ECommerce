30b75bce5d81b74fe40a1598e917e43dbe51ccfc
behzadmobcom <behzadmobcom@gmail.com>
2024-02-12 21:22:17 +0330

    close#465NewColorTest

---
+++ ECommerce.Repository.UnitTests/Colors/ColorAddTest.cs
@@ -0,0 +1,64 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Interfaces;
+using ECommerce.Infrastructure.DataContext;
+using ECommerce.Infrastructure.Repository;
+using ECommerce.Repository.UnitTests.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Imaging;
+using Xunit;
+
+namespace ECommerce.Repository.UnitTests.Colors;
+
+public class ColorAddTest : BaseTests
+{
+    private IColorRepository _colorRepository;
+    private readonly CancellationToken _cancellationToken;
+
+    public ColorAddTest()
+    {
+        DbContextFake db = new DbContextFake();
+        //SunflowerECommerceDbContext dbContext = db.GetDatabaseContext();
+        _colorRepository = new ColorRepository(DbContext);
+        _cancellationToken = new CancellationToken();
+    }
+       
+    [Fact]
+    public async Task Add_AddNewEntity_ReturnsSameEntity()
+    {
+        //Arrange
+        var id = 1001;
+        var name = Guid.NewGuid().ToString();
+        var colorCode = Guid.NewGuid().ToString();
+        var expectedColor = new Color
+        {
+            Id = id,
+            Name = name,
+            ColorCode = colorCode
+        };
+
+        //Act
+        _colorRepository.Add(expectedColor);
+        await UnitOfWork.SaveAsync(CancellationToken);
+        Color actualColor = DbContext.Colors.Where(c => c.Id == id).First();
+
+        //Assert
+        Assert.Equal(expectedColor.Id, actualColor.Id);
+        Assert.Equal(expectedColor.Name, actualColor.Name);
+        Assert.Equal(expectedColor.ColorCode, actualColor.ColorCode);
+    }
+
+    [Fact]
+    public void Add_AddNullEntity_ReturnsException()
+    {
+        //Arrange
+        Color expectedColor = new();
+
+        //Act
+
+        //Assert
+        Assert.Throws<DbUpdateException>(() => _colorRepository.Add(expectedColor));
+    }
+
+    
+
+}
---
+++ ECommerce.Repository.UnitTests/Colors/ColorDeleteTest.cs
@@ -0,0 +1,78 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Domain.Interfaces;
+using ECommerce.Infrastructure.DataContext;
+using ECommerce.Infrastructure.Repository;
+using ECommerce.Repository.UnitTests.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Imaging;
+using Xunit;
+
+namespace ECommerce.Repository.UnitTests.Colors;
+
+public class ColorDeleteTest : BaseTests
+{
+    private IColorRepository _colorRepository;
+    private readonly CancellationToken _cancellationToken;
+
+    public ColorDeleteTest()
+    {
+        DbContextFake db = new DbContextFake();
+        //SunflowerECommerceDbContext dbContext = db.GetDatabaseContext();
+        _colorRepository = new ColorRepository(DbContext);
+        _cancellationToken = new CancellationToken();
+    }
+       
+    [Fact]
+    public async Task Delete_DeleteEntity_ReturnsNull()
+    {
+        //Arrange
+        var id = 1003;
+        var name = Guid.NewGuid().ToString();
+        var colorCode = Guid.NewGuid().ToString();
+        var color = new Color
+        {
+            Id = id,
+            Name = name,
+            ColorCode = colorCode
+        };
+        DbContext.Colors.Add(color);
+        DbContext.SaveChanges();
+        DbContext.ChangeTracker.Clear();
+
+        //Act
+        _colorRepository.Delete(color);
+        await UnitOfWork.SaveAsync(CancellationToken);
+        var actualColor = DbContext.Colors.Where(c => c.Id == id).FirstOrDefault();
+
+        //Assert
+        Assert.Null(actualColor);
+    }
+
+    [Fact]
+    public async Task Delete_DeleteEntity_ReturnsZeroCount()
+    {
+        //Arrange
+        int expectedCount = 0;
+        var id = 1004;
+        var name = Guid.NewGuid().ToString();
+        var colorCode = Guid.NewGuid().ToString();
+        var color = new Color
+        {
+            Id = id,
+            Name = name,
+            ColorCode = colorCode
+        };
+        DbContext.Colors.Add(color);
+        DbContext.SaveChanges();
+        DbContext.ChangeTracker.Clear();
+
+        //Act
+        _colorRepository.Delete(color);
+        await UnitOfWork.SaveAsync(CancellationToken);
+        int actualColor = DbContext.Colors.Count();
+
+        //Assert
+        Assert.Equal(expectedCount, actualColor);
+    }
+
+}
